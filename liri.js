fd64a60b324852850a161628d6223a2befe92137
Ashley Clark <debashley06@gmail.com>
2019-10-26 23:01:30 -0400

    added working functions

--- liri.js
+++ liri.js
@@ -1,16 +1,22 @@
-require("dotenv").config();
 
-var keys = require("./keys.js");
-var fs = require("fs");
-var request = require("request")
-var Spotify = require('node-spotify-api');
+//require for node modules and npm's
+const keys = require('./keys.js');
+const fs = require('fs');
+const request = require('request');
+const spotify = require('node-spotify-api');
+const axios = require('axios');
+const dotenv = require('dotenv').config();
+const moment = require('moment');
 
-var spotify = new Spotify (keys.spotify);
+//Spotify key
+//var spotify = new Spotify (keys.spotify);
 
-//Spotify
 
 var userPrompt = function(userCommand){
-    switch(userCommand){
+
+    userCommand = process.argv[2];  
+ 
+ switch(userCommand){
         case "concert-this":
             searchConcert();
             break;
@@ -25,6 +31,8 @@ var userPrompt = function(userCommand){
             break;
     }
 }
+userPrompt();
+
 var searchSpotify = function (songName){
     if (songName === undefined){
         songName = "The Sign";
@@ -34,29 +42,47 @@ var searchSpotify = function (songName){
 
     console.log(`Artist: ${artist} \nSong Name: ${song} \nAlbum: ${album} \n Preview song here: ${alink}`);
 
-    }
+}
 
-var searchMovie = function(movieName){
-    if(movieName === undefined){
-        movieName = "Mr. Nobody";
-    } else {
-        //OMDB API call
-    }
-    
-console.log(`Title of the movie: ${title} \nYear the movie came out: ${year} \nIMDB Rating of the movie: ${rating}\nRotten Tomatoes Rating: ${rtrating}\nCountry where the movie was produced: ${country} \nLanguage: ${language} \nPlot: ${plot} \nActors: ${actors}`);
+function searchMovie (movieName){
+    movieName = process.argv[3];
+
+    if (movieName === undefined){
+        movieName = "Mr. Nobody";  
+    } ; 
+
+    var queryURL = "https://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+
+    axios({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) { 
+        //console.log(process.argv[3])
+        //console.log(response.data);
+       console.log(`Title of the movie: ${response.data.Title} \nYear the movie came out: ${response.data.Year} \nIMDB Rating of the movie: ${response.data.Rated}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry where the movie was produced: ${response.data.Country} \nLanguage: ${response.data.Language} \nPlot: ${response.data.Plot} \nActors: ${response.data.Actors}`);
+
+});
 
 }
 
-var searchConcert = function(concert){
-    //call Boysintown API
-    
-console.log(`Venue name: ${venue} \nVenue Location: ${location} \n Date: ${date}`);
 
-//use moment to format this as "MM/DD/YYYY")
-//moment().format('L');
+function searchConcert(artist){
 
+    artist = process.argv[3];
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+    var date= response.data[0].datetime 
+    var momentDate= moment(date).format('L')
+   // console.log(momentDate)
+   console.log(`Artist: ${response.data[0].venue.name} \nVenue Location: ${response.data[0].venue.city} \nDate: ${momentDate}`);
+
+});
 }
 
-var doSay = function(){
-
-}
+//var doSay = function(){}
